d1fbf8f998972da48e658900e87a7fa1d0c9075c
TwenKey <63151009+TwenKeyGarph@users.noreply.github.com>
2021-06-09 18:33:38 +0300

    upd

---
+++ common/commands/archive/fifteen-game.js
@@ -0,0 +1,12 @@
+// consts
+
+// export
+module.exports = {
+    sysname: 'fifteen-game',
+    callout: 'ff',
+    about: 'Awesome game!',
+    example: 'ff [play | leaderboard]',
+    execute(client, message, args) {
+        message.reply('coming soon');
+    },
+};
