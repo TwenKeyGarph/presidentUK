f4a149af408b1aefbabb9663c538bdf86d50747a
TwenKey Garph <vovssokolov2003@gmail.com>
2021-06-22 18:46:35 +0300

    about-cmd-fix

--- common/commands/about-cmd.js
+++ common/commands/about-cmd.js
@@ -8,7 +8,7 @@ module.exports = {
     example: 'help [command]',
     execute(client, message, args) {
         let arg = args[0];
-        if (client.CACHE.has(arg))
+        if (client.CACHE.alias.has(arg))
             message.reply(`${client.getCmdByAlias(arg).aliases[0]} is ${client.getCmdByAlias(arg).about}`)
         else 
             message.reply(`Wrong command (\`${arg}\`)`)
