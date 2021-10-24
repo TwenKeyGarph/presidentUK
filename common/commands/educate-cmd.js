afc6a7dc7df6edfbda1bb6e905ae624b3d66fb36
TwenKey Garph <vovssokolov2003@gmail.com>
2021-10-24 13:03:45 +0300

    cmd educate

---
+++ common/commands/educate-cmd.js
@@ -0,0 +1,14 @@
+// consts
+const i18n = require('i18n');
+
+// export
+module.exports = {
+    name: 'educate-cmd',
+    aliases: ['educate', 'edu'],
+    example: 'edu',
+    execute(client, message, args) {
+        const locale = message.author.loc;
+
+        
+    },
+};
